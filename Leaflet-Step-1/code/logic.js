86cfed09d956cf241cb4a54a29a526709c0a95ee
Travis Lucas <fastlife.future@Traviss-MacBook-Air.local>
2022-04-24 13:19:03 -0400

    created files for step 1

---
+++ Leaflet-Step-1/code/logic.js
@@ -0,0 +1,11 @@
+var myMap = L.map("map", {
+    center: [95.7129, 37.0902],
+    zoom: 2
+  });
+  
+  // Adding a tile layer (the background map image) to our map:
+  // We use the addTo() method to add objects to our map.
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(myMap);
+//   37.0902, 95.7129
